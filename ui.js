39bae8b4f3456b16c3ae05af208a022fc2592855
ganer <ganeralkidz@gmail.com>
2021-06-16 18:31:56 -0400

    handled emojis and symbols

--- ui.js
+++ ui.js
@@ -1,23 +1,29 @@
 const grid = document.getElementById("grid");
 
-for (let i = 0; i < 100; i++) {
-  grid.innerHTML += "<div class='cell' maxLength=1 minLength=1 contenteditable=true>.</textarea>";
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++){
+    grid.innerHTML += `<div class='cell' id='(${i},${j})' contenteditable=true>.</textarea>`;
+  }
+  
 }
 
 const cell = document.querySelectorAll(".cell");
-console.log(cell);
 
 for (let c of cell) {
   c.addEventListener("keydown", (event) => {
-		event.preventDefault()
+    event.preventDefault();
     if (event.key === "Backspace") {
       c.textContent = ".";
-    } else if (event.key === "Enter") {
-      c.blur()
-    } else if (/[\w]/.test(event.key)) {
-      console.log("hello");
+    } else if (/^[^\s]$/i.test(event.key)){
       c.textContent = event.key;
       console.log(c.textContent);
+    } else if (event.key !== 'Shift' && event.key !== 'Control' && event.key !== 'Alt' && event.key !== 'Meta') {
+      c.blur();
     }
   });
+  c.addEventListener('input', () => {
+    if (c.textContent.length > 1) {
+      c.textContent = '.'
+    }
+  })
 }
