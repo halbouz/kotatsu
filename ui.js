bad66985ac77a0b2c78c8d893c8de2f1ec5c550d
ganer <ganeralkidz@gmail.com>
2021-06-17 19:42:37 -0400

    fixed bugs

--- ui.js
+++ ui.js
@@ -1,28 +1,45 @@
 const grid = document.getElementById("grid");
+let isCtrlPressed = false;
 
 for (let i = 0; i < 10; i++) {
   for (let j = 0; j < 10; j++){
-    grid.innerHTML += `<div class='cell' id='(${i},${j})' contenteditable=true>.</textarea>`;
+    grid.innerHTML += `<div class='cell' id='(${i},${j})' contenteditable=true>.</div>`;
   }
-  
 }
+
+document.addEventListener('keydown', (event) => {
+  if (event.ctrlKey) {
+    isCtrlPressed = true;
+  }
+})
+
+document.addEventListener('keyup', (event) => {
+  if (!event.ctrlKey) {
+    isCtrlPressed = false;
+  }
+})
 
 const cell = document.querySelectorAll(".cell");
 
 for (let c of cell) {
   c.addEventListener("keydown", (event) => {
-    event.preventDefault();
-    if (event.key === "Backspace") {
+    if (event.key === "Backspace" || event.key === 'Delete') {
+      event.preventDefault();
       c.textContent = ".";
     } else if (/^[^\s]$/i.test(event.key)){
-      c.textContent = event.key;
-      console.log(c.textContent);
-    } else if (event.key !== 'Shift' && event.key !== 'Control' && event.key !== 'Alt' && event.key !== 'Meta') {
+      if (!isCtrlPressed) {
+        c.textContent = event.key;
+        console.log(c.textContent);
+      }
+    } else if (event.key !== 'Shift' && event.key !== 'Control' && event.key !== 'Alt' && event.key !== 'Meta' && !(/^F[1-9]+/.test(event.key))) {
       c.blur();
     }
   });
+  c.addEventListener('beforeinput', () => {
+    c.textContent = ''
+  })
   c.addEventListener('input', () => {
-    if (c.textContent.length > 1) {
+    if (c.textContent.length > 5) {
       c.textContent = '.'
     }
   })
