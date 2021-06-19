8e0c6345170b610d45e649bb503ccd39a893ea24
ganer <ganeralkidz@gmail.com>
2021-06-19 18:51:11 -0400

    added post request needs fixing

--- ui.js
+++ ui.js
@@ -1,61 +1,78 @@
 window.addEventListener("load", async () => {
-
-  const response = await fetch('https://kotatsu-server-silk.vercel.app/api/board')
-  const board = await response.json()
-  console.log(board)
+  const response = await fetch(
+    "https://kotatsu-server-silk.vercel.app/api/board"
+  );
+  const board = await response.json();
+  console.log(board);
 
   const grid = document.getElementById("grid");
   let isCtrlPressed = false;
-
-  for (let i = 1; i < 11; i++) {
-    for (let j = 1; j < 11; j++) {
-      let coord = board[`${i}_${j}`]
-      grid.innerHTML += `<div class='cell' id='${i}_${j}' contenteditable=true>${coord}</div>`;
-    }
-  }
 
   document.addEventListener("keydown", (event) => {
     if (event.ctrlKey) {
       isCtrlPressed = true;
     }
   });
-  
+
   document.addEventListener("keyup", (event) => {
     if (!event.ctrlKey) {
       isCtrlPressed = false;
     }
   });
-  
-  const cell = document.querySelectorAll(".cell");
-  
-  for (let c of cell) {
-    c.addEventListener("keydown", (event) => {
-      if (event.key === "Backspace" || event.key === "Delete") {
-        event.preventDefault();
-        c.textContent = ".";
-      } else if (/^[^\s]$/i.test(event.key)) {
-        if (!isCtrlPressed) {
-          c.textContent = event.key;
-          console.log(c.textContent);
-        }
-      } else if (
-        event.key !== "Shift" &&
-        event.key !== "Control" &&
-        event.key !== "Alt" &&
-        event.key !== "Meta" &&
-        !/^F[0-9]+$/.test(event.key)
-      ) {
-        c.blur();
-      }
-    });
-    c.addEventListener("beforeinput", () => {
-      c.textContent = "";
-    });
-    c.addEventListener("input", () => {
-      if (c.textContent.length > 5) {
-        c.textContent = ".";
-      }
-    });
+
+  for (let i = 1; i < 11; i++) {
+    for (let j = 1; j < 11; j++) {
+      let coord = board[`${i}_${j}`];
+      grid.innerHTML += `<div class='cell' id='${i}_${j}' contenteditable=true>${coord}</div>`;
+    }
   }
-  
+
+  for (let i = 1; i < 11; i++) {
+    for (let j = 1; j < 11; j++) {
+      let c = document.getElementById(`${i}_${j}`);
+      c.addEventListener("keydown", (event) => {
+        if (event.key === "Backspace" || event.key === "Delete") {
+          event.preventDefault();
+          c.textContent = ".";
+        } else if (/^[^\s]$/i.test(event.key)) {
+          if (!isCtrlPressed) {
+            c.textContent = event.key;
+          }
+        } else if (
+          event.key !== "Shift" &&
+          event.key !== "Control" &&
+          event.key !== "Alt" &&
+          event.key !== "Meta" &&
+          !/^F[0-9]+$/.test(event.key)
+        ) {
+          c.blur();
+        }
+      });
+      c.addEventListener("beforeinput", () => {
+        if (!isCtrlPressed) {
+          c.textContent = "";
+        }
+      });
+      c.addEventListener("input", () => {
+        if (c.textContent.length > 5) {
+          c.textContent = ".";
+        }
+        console.log(c.textContent)
+        fetch(`https://kotatsu-server-silk.vercel.app/api/${i}_${j}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "text/plain",
+          },
+          body: c.textContent,
+        })
+          .then((res) => res.text())
+          .then((data) => {
+            console.log("Success:", data);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      });
+    }
+  }
 });
