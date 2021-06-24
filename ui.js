236d21da0ce3857bd18767052bf39133c73336b4
ganer <ganeralkidz@gmail.com>
2021-06-24 17:54:30 -0400

    fixed before input

--- ui.js
+++ ui.js
@@ -35,16 +35,19 @@ window.addEventListener("load", async () => {
         if (event.key === "Backspace" || event.key === "Delete") {
           event.preventDefault();
           c.textContent = ".";
-          const response = await fetch(`https://kotatsu-server-silk.vercel.app/api/${i}_${j}`, {
-            method: "POST",
-            headers: {
-              'Accept': 'application/json',
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              char: '.'
-            }),
-          });
+          const response = await fetch(
+            `https://kotatsu-server-silk.vercel.app/api/${i}_${j}`,
+            {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                char: ".",
+              }),
+            }
+          );
           const success = await response.json();
           console.log(success);
         } else if (/^[^\s]$/i.test(event.key)) {
@@ -62,46 +65,50 @@ window.addEventListener("load", async () => {
         }
       });
       c.addEventListener("beforeinput", () => {
-        if (!isCtrlPressed) {
-          c.textContent = "";
-        }
+        c.textContent = "";
       });
       c.addEventListener("input", async () => {
         if (c.textContent.length > 5) {
           c.textContent = ".";
         }
-        console.log(c.textContent)
+        console.log(c.textContent);
         console.log(`${i}_${j}`);
-        const response = await fetch(`https://kotatsu-server-silk.vercel.app/api/${i}_${j}`, {
-          method: "POST",
-          headers: {
-            'Accept': 'application/json',
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            char: c.textContent
-          }),
-        });
+        const response = await fetch(
+          `https://kotatsu-server-silk.vercel.app/api/${i}_${j}`,
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              char: c.textContent,
+            }),
+          }
+        );
         const success = await response.json();
         console.log(success);
       });
     }
   }
 
-  const reset_button = document.getElementById('reset');
-  reset_button.addEventListener('click', async () => {
+  const reset_button = document.getElementById("reset");
+  reset_button.addEventListener("click", async () => {
     for (let i = 1; i < 11; i++) {
       for (let j = 1; j < 11; j++) {
         let c = document.getElementById(`${i}_${j}`);
-        c.textContent = '.'
+        c.textContent = ".";
       }
     }
-    const response = await fetch(`https://kotatsu-server-silk.vercel.app/api/board`, {
-      method: "POST",
-      headers: {},
-      body: {}
-    });
+    const response = await fetch(
+      `https://kotatsu-server-silk.vercel.app/api/board`,
+      {
+        method: "POST",
+        headers: {},
+        body: {},
+      }
+    );
     const success = await response.json();
     console.log(success);
-  })
+  });
 });
